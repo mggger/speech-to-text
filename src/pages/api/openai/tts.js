353dd2c581652c47e0cab33ddbd137e124e5a3e5
王家敏 <mggis0or1@gmail.com>
2024-01-20 00:24:11 +0800

    update

--- src/pages/api/openai/tts.js
+++ src/pages/api/openai/tts.js
@@ -2,6 +2,8 @@ import FormData from 'form-data';
 import formidable from 'formidable';
 import fs from 'fs';
 import axios from 'axios';
+import {HttpsProxyAgent} from 'https-proxy-agent';
+
 
 export const config = {
     api: {
@@ -28,27 +30,39 @@ export default async function handler(req, res) {
         });
 
         const audioFile = files.file && files.file.length > 0 ? files.file[0].filepath : null;
+        const newFilePath = audioFile + '.mpeg';
+        if (audioFile) {
+            // 设置新的文件路径，包括 .mpeg 扩展名
 
-        if (!audioFile) {
-            return res.status(400).json({error: 'No audio file found'});
+            // 重命名文件以包含正确的扩展名
+            fs.rename(audioFile, newFilePath, (err) => {
+                if (err) {
+                    console.error('Error renaming file:', err);
+                } else {
+                    console.log('File renamed successfully to:', newFilePath);
+                    // 在这里继续处理新文件路径（例如，上传到 OpenAI API）
+                }
+            });
+        } else {
+            console.log('No audio file found');
         }
 
         const formData = new FormData();
-        formData.append('file', fs.createReadStream(audioFile));
+        formData.append('file', fs.createReadStream(newFilePath));
         formData.append('model', 'whisper-1');
 
-        const openaiApiKey = process.env.API_KEY; // 更改为非公开环境变量
+        const openaiApiKey = process.env.NEXT_PUBLIC_API_KEY; // 更改为非公开环境变量
 
         const response = await axios.post('https://api.openai.com/v1/audio/transcriptions', formData, {
             headers: {
                 ...formData.getHeaders(),
                 'Authorization': `Bearer ${openaiApiKey}`,
-            },
+            }
         });
 
-        res.status(200).json({transcript: response.data});
+        res.status(200).json({transcript: response.data.text});
     } catch (error) {
-        console.error('Error:', error);
+        console.error(error.response ? error.response.data : error.message);
         res.status(500).json({error: 'Error processing audio file'});
     }
 }
